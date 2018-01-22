b1e457b0210e7e719e91cbf9c63037ba56121443
demonkiss <captain9304@hotmail.com>
2018-01-22 18:20:37 +0800

    second

---
+++ app/hello.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+class Hello extends React.Component{
+    render(){
+        return 
+        (<h1>Hello Robot!</h1>)  
+    }
+    
+    
+    
+}
+
+
+
+export default Hello
